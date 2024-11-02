c06727a4a6c65e4068f1e1d2b91d525ab398a877
hayden-donnelly <hayden.git@proton.me>
2024-11-02 11:35:03 -0400

    Added fc kernel launch function and forward pass function.

--- src/mlp_cuda_basic.cu
+++ src/mlp_cuda_basic.cu
@@ -1,7 +1,9 @@
+#include <cuda.h>
 #include <iostream>
 #include <cstdio>
 #include <cuda_runtime.h>
 #include <curand_kernel.h>
+#include "mnist.hpp"
 
 #define CHECK_CUDA(expression) \
 { \
@@ -14,45 +16,20 @@
     } \
 }
 
-// Initialize weights to random values following a normal distribution.
-__global__ void random_normal_init_kernel(float* A, int n_elements, unsigned long seed)
+struct mlp_t
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if(idx < n_elements)
-    {
-        curandState state;
-        curand_init(seed, idx, 0, &state);
-        A[idx] = curand_normal(&state); 
-    } 
-}
-
-void random_normal_init(int height, int width, float* A, unsigned long seed)
-{
-    const int n_elements = height * width;
-    const int block_dim = 1024;
-    const int grid_dim = (n_elements + block_dim - 1) / block_dim;
-    printf("block_dim: %d\ngrid_dim: %d\n", block_dim, grid_dim);
-    random_normal_init_kernel<<<grid_dim, block_dim>>>(A, n_elements, seed);
-}
-
-__global__ void zero_init_kernel(float* A, int n_elements)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if(idx < n_elements)
-    {
-        A[idx] = 0.0f;
-    }
-}
-
-void zero_init(int height, int width, float* A)
-{
-    const int n_elements = height * width;
-    const int block_dim = 1024;
-    const int grid_dim = (n_elements + block_dim - 1) / block_dim;
-    zero_init_kernel<<<grid_dim, block_dim>>>(A, n_elements);
-}
+    int input_dim;
+    int hidden_dim;
+    int output_dim;
+    int batch_size;
+    float* fc1_w;
+    float* fc1_b;
+    float* fc2_w;
+    float* fc2_b;
+    float* input;
+    float* hidden;
+    float* output;
+};
 
 template<int tile_width>
 __global__ void fc_forward_kernel(
@@ -108,6 +85,77 @@ __global__ void fc_forward_kernel(
     }
 }
 
+template<int tile_width> 
+void fc_forward(
+    const float* W, const float* X, float* Y,
+    int input_dim, int output_dim, int batch_size 
+){
+    // batch is vertical
+    // hidden dim is horizontal
+    const int block_size = 32;
+    dim3 grid_dim((output_dim + tile_width - 1) / tile_width, (batch_size + tile_width - 1) / tile_width);
+    dim3 block_dim(tile_width, tile_width);
+    fc_forward_kernel<tile_width><<<grid_dim, block_dim>>>(W, X, Y, input_dim, output_dim, batch_size);
+}
+
+template<int tile_width> 
+void forward_pass(mlp_t* mlp)
+{
+    fc_forward<tile_width>(
+        (const float*)mlp->fc1_w, (const float*)mlp->input, mlp->hidden,
+        mlp->input_dim, mlp->hidden_dim, mlp->batch_size
+    );
+    // TODO:
+    // bias
+    // relu
+    fc_forward<tile_width>(
+        (const float*)mlp->fc2_w, (const float*)mlp->hidden, mlp->output,
+        mlp->hidden_dim, mlp->output_dim, mlp->batch_size
+    );
+    // TODO:
+    // softmax
+}
+
+// Initialize weights to random values following a normal distribution.
+__global__ void random_normal_init_kernel(float* A, int n_elements, unsigned long seed)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(idx < n_elements)
+    {
+        curandState state;
+        curand_init(seed, idx, 0, &state);
+        A[idx] = curand_normal(&state); 
+    } 
+}
+
+void random_normal_init(int height, int width, float* A, unsigned long seed)
+{
+    const int n_elements = height * width;
+    const int block_dim = 1024;
+    const int grid_dim = (n_elements + block_dim - 1) / block_dim;
+    //printf("block_dim: %d\ngrid_dim: %d\n", block_dim, grid_dim);
+    random_normal_init_kernel<<<grid_dim, block_dim>>>(A, n_elements, seed);
+}
+
+__global__ void zero_init_kernel(float* A, int n_elements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(idx < n_elements)
+    {
+        A[idx] = 0.0f;
+    }
+}
+
+void zero_init(int height, int width, float* A)
+{
+    const int n_elements = height * width;
+    const int block_dim = 1024;
+    const int grid_dim = (n_elements + block_dim - 1) / block_dim;
+    zero_init_kernel<<<grid_dim, block_dim>>>(A, n_elements);
+}
+
 void device_to_host_and_print(int height, int width, float* d_A)
 {
     size_t mat_size = sizeof(float) * height * width;
@@ -127,33 +175,35 @@ void device_to_host_and_print(int height, int width, float* d_A)
 
 int main()
 {
-    constexpr int64_t input_dim = 784;
-    constexpr int64_t hidden_dim = 256;
-    constexpr int64_t output_dim = 10;
-    constexpr int64_t batch_size = 32;
+    load_mnist();
+    printf("Loaded MNIST\n");
+    print_image(train_image[2]);
 
-    float* d_fc1_weights;
-    float* d_fc2_weights;
-    float* d_fc1_bias;
-    float* d_fc2_bias;
-    float* d_input;
-    float* d_hidden;
-    float* d_output;
+    constexpr int input_dim = 784;
+    constexpr int hidden_dim = 256;
+    constexpr int output_dim = 10;
+    constexpr int batch_size = 32;
 
-    CHECK_CUDA(cudaMalloc(&d_fc1_weights, sizeof(float) * input_dim * hidden_dim));
-    CHECK_CUDA(cudaMalloc(&d_fc2_weights, sizeof(float) * hidden_dim * output_dim));
-    CHECK_CUDA(cudaMalloc(&d_fc1_bias, sizeof(float) * hidden_dim));
-    CHECK_CUDA(cudaMalloc(&d_fc2_bias, sizeof(float) * output_dim));
-    CHECK_CUDA(cudaMalloc(&d_input, sizeof(float) * batch_size * input_dim));
-    CHECK_CUDA(cudaMalloc(&d_hidden, sizeof(float) * batch_size * hidden_dim));
-    CHECK_CUDA(cudaMalloc(&d_output, sizeof(float) * batch_size * output_dim));
+    mlp_t mlp;
+    CHECK_CUDA(cudaMalloc(&mlp.fc1_w, sizeof(float) * input_dim * hidden_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.fc2_w, sizeof(float) * hidden_dim * output_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.fc1_b, sizeof(float) * hidden_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.fc2_b, sizeof(float) * output_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.input, sizeof(float) * batch_size * input_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.hidden, sizeof(float) * batch_size * hidden_dim));
+    CHECK_CUDA(cudaMalloc(&mlp.output, sizeof(float) * batch_size * output_dim));
 
     // Initialize weights and biases.
-    random_normal_init(hidden_dim, input_dim, d_fc1_weights, 0);
-    random_normal_init(hidden_dim, output_dim, d_fc2_weights, 0);
-    zero_init(1, hidden_dim, d_fc1_bias);
-    zero_init(1, output_dim, d_fc2_bias);
-    device_to_host_and_print(output_dim, hidden_dim, d_fc2_weights);
+    random_normal_init(hidden_dim, input_dim, mlp.fc1_w, 0);
+    random_normal_init(hidden_dim, output_dim, mlp.fc2_w, 0);
+    zero_init(1, hidden_dim, mlp.fc1_b);
+    zero_init(1, output_dim, mlp.fc2_b);
+    printf("Initialized weights and biases\n");
+    //device_to_host_and_print(output_dim, hidden_dim, mlp.fc2_w);
 
-    printf("Hello World\n");
+    cudaMemcpy(
+        mlp.input, &train_image[0], 
+        sizeof(float) * input_dim * batch_size, cudaMemcpyHostToDevice
+    );
+    device_to_host_and_print(batch_size, input_dim, mlp.input);
 }
