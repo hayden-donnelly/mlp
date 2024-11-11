747f42aa3331296ca0e5cbad53219215e21320ec
hayden-donnelly <donnellyhd@outlook.com>
2024-11-11 16:22:10 -0500

    Added unit test for cross_entropy_forward.

--- src/matrix_utils.cpp
+++ src/matrix_utils.cpp
@@ -15,6 +15,20 @@ float* make_random_matrix(int n_elements)
     return mat;
 }
 
+int64_t* make_random_labels(int n_labels)
+{
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int64_t> dist(0, 9);
+    
+    int64_t* labels = (int64_t*)malloc(sizeof(int64_t) * n_labels);
+    for(int i = 0; i < n_labels; ++i)
+    {
+        labels[i] = dist(rng); 
+    }
+    return labels;
+}
+
 bool matrices_are_equal(float* A, float* B, int n_elements, float eps)
 {
     for(int i = 0; i < n_elements; ++i)
--- src/matrix_utils.hpp
+++ src/matrix_utils.hpp
@@ -2,3 +2,4 @@
 
 float* make_random_matrix(int n_elements);
 bool matrices_are_equal(float* A, float* B, int n_elements, float eps);
+int64_t* make_random_labels(int n_labels);
