a7ced9acc06bc0f6f038653bb2f0f22065a8e9f0
hayden-donnelly <hayden.git@proton.me>
2024-11-13 12:14:59 -0500

    Added unit test for cross_entropy_backward.

--- src/matrix_utils.hpp
+++ src/matrix_utils.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-float* make_random_matrix(int n_elements);
+float* make_random_matrix(int n_elements, float min, float max);
 bool matrices_are_equal(float* A, float* B, int n_elements, float eps);
 int64_t* make_random_labels(int n_labels);
